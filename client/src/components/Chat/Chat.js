8245907578c89051d6d30c6e28f642903575bcca
GLEDSONS831 <glegogle84@gmail.com>
2020-05-26 07:31:05 -0300

    :pencil2: config socket.io

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,14 +3,26 @@ import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+let socket;
+
 const Chat = ( {location} ) =>{
 
-    useEffect(() =>{
-        const data = queryString.parse(location.search);
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
 
-        console.log(location.search);
-        console.log(data)
-    })
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() =>{
+        const {name, room} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+        
+        socket.emit('join', {name: name, room: room});
+
+    }, [ENDPOINT, location.search ])
 
     return (
         <h1>Chat</h1>

d9ad1e9e9297282b60bb87ce66a7ba17ccd764ec
GLEDSONS831 <glegogle84@gmail.com>
2020-05-26 00:47:58 -0300

    :pencil2: add functions chat

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+const app = express();
+const port = process.env.PORT || 5000;
+const router = require('./router');
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+io.on('connection', (socket) =>{
+    console.log('Connection')
+
+    socket.on('disconnect', () =>{
+        console.log("fffffffffffffff")
+    })
+})
+
+app.use(router);
+
+server.listen(port, () =>{
+    console.log(`port: ${port}`)
+})

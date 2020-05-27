eae0414175abbe13240d22c396cc051d4e514bc3
GLEDSONS831 <glegogle84@gmail.com>
2020-05-27 03:52:08 -0300

    :pencil2: add user functions

--- server/index.js
+++ server/index.js
@@ -4,18 +4,33 @@ const http = require('http');
 const app = express();
 const port = process.env.PORT || 5000;
 const router = require('./router');
-
 const server = http.createServer(app);
-
 const io = socketIo(server);
 
+const { addUser, removeUser, getUser, getUserInRoom} = require('./users');
+
 io.on('connection', (socket) =>{
-    console.log('Connection')
+   
 
     socket.on('join', ({name, room}, callback)=>{
-        console.log(name, room)
+        const {error, user} = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error)
+
+        socket.emit('mesage', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
+        socket.broadcast.to(user.room).emit('message', { user:'admin', text: `${user.name}, has joined` });
+
+        socket.join(user.room);
+
+        callback();
     });
 
+    socket.on('sendMessage', (message, callback) =>{
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message})
+    })
+    callback();
     socket.on('disconnect', () =>{
         console.log("fffffffffffffff")
     })
